b6ccc66c6b67ac19e142f21e5a4a7d253d4cbc54
BCS <christiansuinyuy@gmail.com>
2025-09-13 11:11:39 +0100

    inserting footer to all pages from js

--- scripts/chris.js
+++ scripts/chris.js
@@ -162,7 +162,6 @@ let travels = [
 ]
 
 function insertDetails(details){
-    console.log(details)
     let html = `
         <img src='${details.images[0]}' class="w-70 rounded-2xl md:row-span-2 row-start-2" alt="tor image">
                 <div class="details col-span-2 md:col-start-2 md:col-span-1  grid gap-0.5 ">
@@ -242,11 +241,60 @@ function insertItinarery(details){
 function insertsGalery(details){
     let html = ''
     details.images.forEach(image=>{
-        html+= `<img src="${image}" alt=""></img>`
+        html+= `<img src="${image}" alt="" class= "w-full h-50"></img>`
     })
 
     let galleryElem = document.getElementById('gallery')
     galleryElem.innerHTML = html
+}
+
+function insertFooter(){
+    let FooterElem = document.getElementById('footer')
+    console.log(FooterElem)
+    FooterElem.innerHTML = `
+    <div class="grid grid-flow-col grid-row-4 bg-white gap-4 font-light mt-32">
+        <div >
+                <h1 class="font-bold ml-4">Follow us</h1>
+                <i class="fa-brands fa-facebook"></i>
+                 <i class="fa-solid fa-xmark"></i>
+                <i class="fa-brands fa-instagram"></i>
+             </div>
+             <div>
+            <ul>
+                <h1 class="font-bold">international flights</h1>
+                <li><a href="">international flights</a></li>
+                
+          </ul>
+             </div>
+             <div>
+                <h1 class="font-bold">Company</h1>
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Term & Condition</a></li>
+                    <li><a href="">Privacu policy</a></li>
+                    <li><a href="">Booking Rules</a></li>
+                </ul>
+             </div>
+             <div>
+                <h1 class="font-bold">Address</h1>
+                <i class="fa-solid fa-location-dot font-light"></i>
+                Korem ipsum sit amet, consectetur<br>
+                adipisci dolor sit amet, cong elit
+                <h1 class="font-bold">Phone</h1>
+                <ul>
+                    
+                    <li> <i class="fa-solid fa-mobile"></i>01-4821579</li>
+                    <li><i class="fa-solid fa-mobile"></i>(+237) 673639542</li>
+                </ul>
+                <h1 class="font-bold mt-4">CHAT WITH US ON</h1>
+                <span> <i class="fa-brands fa-viber "></i>Viber</span><br>
+                <span><i class="fa-brands fa-whatsapp"></i>Whats App</span>
+                <h1 class="font-bold">Mail</h1>
+                <span><i class="fa-solid fa-envelope"></i>thuk@thuktravels.com</span>
+             </div>
+            </div>
+    `
 }
 
 function renderPage(Tour){
@@ -254,5 +302,6 @@ function renderPage(Tour){
     insertsGalery(Tour)
     insertItinarery(Tour)
 }
+insertFooter()
 
-renderPage(Tour[2])
+renderPage(Tour[0])
